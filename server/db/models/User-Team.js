253d0b45eae3e37e87b2f2172ef66a016ed46d43
Marco Kwan <marcokwan10@gmail.com>
2022-05-13 02:06:02 -0400

    added id to user-to-tem model

--- server/db/models/User-Team.js
+++ server/db/models/User-Team.js
@@ -1,6 +1,13 @@
 const { DataTypes } = require("sequelize");
 const db = require("../db");
 
-const UserTeam = db.define("userToTeam");
+const UserTeam = db.define("user-team", {
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+});
 
 module.exports = UserTeam;
